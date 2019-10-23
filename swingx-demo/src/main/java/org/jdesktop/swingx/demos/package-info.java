a6d220498e258639edf7363bc70ff9738626dd94
homebeaver <eugen.hanussek@klst.com>
2019-10-23 12:14:29 +0200

    Beispiele zu swingset 3
    - diese lassen sich nicht separat starten - nur mit dem kompletten
    SwingLab Demos, launcher: RUN SwingSet 3

---
+++ swingx-demo/src/main/java/org/jdesktop/swingx/demos/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Beispiele zu swingset 3
+ * diese lassen sich nicht separat starten - nur mit dem kompletten SwingLab Demos, launcher: RUN SwingSet 3
+ * 
+ * XTreeDemo     wie org.jdesktop.swingx.demos.tree.XTreeDemo
+ * TreeTableDemo wie org.jdesktop.swingx.demos.treetable.TreeTableDemo
+ */
+package org.jdesktop.swingx.demos;
