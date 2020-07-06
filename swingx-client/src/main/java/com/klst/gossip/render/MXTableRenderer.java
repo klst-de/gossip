da85466cd2011ecc2b3110a885be94be5cbe340f
homebeaver <eugen.hanussek@klst.com>
2020-07-06 09:11:35 +0200

    MXTableRenderer implementiert Methode getTableCellRendererComponent
    für Zelle int row, int column

---
+++ swingx-client/src/main/java/com/klst/gossip/render/MXTableRenderer.java
@@ -0,0 +1,133 @@
+package com.klst.gossip.render;
+
+import java.awt.Component;
+import java.util.logging.Logger;
+
+import javax.swing.JTable;
+
+import org.compiere.model.GridField;
+import org.compiere.model.GridTable;
+import org.compiere.model.MRefList;
+import org.compiere.util.DisplayType;
+import org.compiere.util.Env;
+import org.compiere.util.ValueNamePair;
+import org.jdesktop.swingx.renderer.ComponentProvider;
+import org.jdesktop.swingx.renderer.DefaultTableRenderer;
+import org.jdesktop.swingx.renderer.JRendererCheckBox;
+import org.jdesktop.swingx.renderer.JRendererLabel;
+import org.jdesktop.swingx.renderer.LabelProvider;
+import org.jdesktop.swingx.renderer.StringValue;
+import org.jdesktop.swingx.table.DefaultTableColumnModelExt;
+
+import com.klst.gossip.MXTable;
+
+public class MXTableRenderer extends DefaultTableRenderer { // extends (swingx)AbstractRenderer implements (swing)TableCellRenderer
+
+	private static final long serialVersionUID = -1911708055572460800L;
+	private static final Logger LOG = Logger.getLogger(MXTableRenderer.class.getName());
+
+//	GridTable gridTableModel; // TODO brauche ich es? raus
+//	GridField[] fields;
+	
+    public MXTableRenderer(GridTable gtModel) {
+    	super();
+//    	gridTableModel = gtModel;
+//    	fields = gtModel.getFields();
+    }
+  
+/*
+
+in package org.jdesktop.swingx.renderer gibt es folgende renderer
+-                 abstract class ComponentProvider<T extends JComponent>
+- class LabelProvider    extends ComponentProvider<JLabel> : A component provider which uses a JLabel as rendering component.
+- class CheckBoxProvider extends ComponentProvider<AbstractButton>: A component provider which uses a JCheckBox. Nicht nur für Boolean! 
+- public class HyperlinkProvider extends ComponentProvider<JXHyperlink> implements RolloverRenderer
+... dann gibt es noch die Klassen in package org.jdesktop.swingx.rollover
+
+ */
+    public MXTableRenderer(StringValue converter) {
+    	super(new LabelProvider(converter));
+    }
+
+//        public MXTableRenderer(StringValue converter, int alignment) {
+//            super(new MyLabelProvider(converter, alignment)); // org.jdesktop.swingx.renderer.LabelProvider.LabelProvider(StringValue converter, int alignment)
+//        }
+        
+    public MXTableRenderer(ComponentProvider<?> componentProvider) {
+        super(componentProvider);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    // methode ist definiert in interface javax.swing.table.TableCellRenderer
+    @Override // implementiert in org.jdesktop.swingx.renderer.DefaultTableRenderer
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+/*
+
+column : nur die Displayed, dh 0 == field 2, da die ersten beden nicht angezeigt werden
+field.getDisplayType | col WorkflowActivities | value.getClass()=Integer  value.getClass()=String  value.getClass()=Boolean
+11                      0N DynPriorityStart       java.lang.Integer                                                          0/0 value:11
+20                      1N IsActive                                       /class java.lang.String
+19                      2  AD_Client_ID          java.lang.Integer
+ */
+    	if(table instanceof MXTable) {
+    		DefaultTableColumnModelExt tcme = (DefaultTableColumnModelExt)(table.getColumnModel());
+    		GridField field = (GridField)(tcme.getColumn(column).getIdentifier());
+    		int displayType = field.getDisplayType();
+        	LOG.config(table 
+        			+ "\n"+column+"/"+row + " value:" + value + " of type " + (value==null ? "null" : value.getClass()) + " Reference_Value_ID:" + field.getAD_Reference_Value_ID()+ " displayType:" + displayType
+//        			+ "\ncomponent:"+component
+        			);
+    		
+			switch(displayType) {
+//			case DisplayType.Date: // 15 Date
+//				field.setDisplayType(DisplayType.Date); // ohne Time
+//				minitable.setColumnClass(f, field);
+//				break;
+//			case DisplayType.DateTime: // 16 DateDoc
+//				field.setDisplayType(DisplayType.DateTime);
+//				minitable.setColumnClass(f, field);
+//				break;
+			case DisplayType.String:   // 10 DocumentNo
+				
+				break;
+//			case DisplayType.ID:       // 13 C_BPartner.C_BPartner_ID
+			case DisplayType.List:     // 17 DocStatus
+				boolean optional = false; //field.isMandatory(checkContext); // auch ein leerer Eintrag ist dabei
+				ValueNamePair[] valueName = MRefList.getList(Env.getCtx(), field.getAD_Reference_Value_ID(), optional);
+				MRefList mRefList = MRefList.get(Env.getCtx(), field.getAD_Reference_Value_ID(), value.toString(), null);
+				LOG.config("mRefList:"+mRefList);
+				JRendererLabel label = new JRendererLabel();
+				label.setText(mRefList.getName()); // ==.toString()
+				return label;
+				//break;
+//			case DisplayType.Table:    // 18 CreatedBy, UpdatedBy
+//			case DisplayType.TableDir: // 19 AD_Client_ID, ...
+//				minitable.setColumnClass(f, field);
+//				break;
+			case DisplayType.YesNo:    // 20
+//				StringValue stringValue = new StringValue() {
+//				     public String getString(Object value) {
+//				         if (value instanceof Boolean)
+//				             return StringValues.TO_STRING.getString(value);
+//				         return value.toString();
+//				     }
+//				};
+//				return new CheckBoxProvider(stringValue);
+				JRendererCheckBox checkbox = new JRendererCheckBox();				
+				checkbox.setSelected((Boolean)value);
+				return checkbox; // TODO ist noch linksbündig
+				//break;
+//			case DisplayType.Location: // 21 Location TODO
+//				field.setDisplayType(DisplayType.TableDir);
+//				minitable.setColumnClass(f, field);
+//				break;
+			default:
+//				minitable.setColumnClass(f, columnClass, displayType, readOnly, header);
+			}
+    	}
+    	return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+    }
+
+}
