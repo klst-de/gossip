0173eb358a90ec813102470ab7965b5ff3b82d2c
homebeaver <homebeaver@outlook.de>
2023-02-09 00:55:55 +0100

    aufräumen + löschen

--- swingx-demo/src/main/java/org/jdesktop/swingx/demos/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Beispiele zu swingset 3
- * diese lassen sich nicht separat starten - nur mit dem kompletten SwingLab Demos, launcher: RUN SwingSet 3
- * 
- * XTreeDemo     wie org.jdesktop.swingx.demos.tree.XTreeDemo
- * TreeTableDemo wie org.jdesktop.swingx.demos.treetable.TreeTableDemo
- */
-package org.jdesktop.swingx.demos;
